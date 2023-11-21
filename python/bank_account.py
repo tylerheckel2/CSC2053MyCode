918bf41e33124290d5ebf9bf7b9cfeba42f9841e
tylerheckel2 <tylerheckel2@gmail.com>
2023-11-21 13:42:11 -0500

    added python files

---
+++ python/bank_account.py
@@ -0,0 +1,26 @@
+# Declare the class BankAccount
+# Python convention is to capitalize first letter in each word of class name
+class BankAccount:
+    # __init__ method is like the constructor in Java
+    # variables declared in the __init__ method are global and MUST
+    # be refereced using self
+    # self is like 'this' in Java
+    def __init__(self, account_number, balance):
+        self.account_number = account_number
+        self.balance = balance
+
+    # __str__ is like the toString() method in Java
+    # it's used for debugging purposes
+    def __str__(self):
+        return self.account_number + ' has the balance ' + str(self.balance)
+
+    # first parameter in class methods MUST be self
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if self.balance > amount:
+            self.balance -= amount
+
+    def get_balance(self):
+        return self.balance
